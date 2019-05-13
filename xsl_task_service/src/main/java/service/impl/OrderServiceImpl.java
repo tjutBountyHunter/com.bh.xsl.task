0ef9a765eb2a79346e5c3899b44f9dbe46ccd6a8
helinhong <tjut_hlh@qq.com>
2019-05-13 17:37:35 +0800

    修复

--- xsl_task_service/src/main/java/service/impl/OrderServiceImpl.java
+++ xsl_task_service/src/main/java/service/impl/OrderServiceImpl.java
@@ -2,10 +2,13 @@ package service.impl;
 
 import mapper.XslOrderMapper;
 import mapper.XslTaskMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import service.OrderService;
+import util.GsonSingle;
 import util.ListUtil;
 import util.UUIDUtil;
 import vo.CreateOrderReqVo;
@@ -22,26 +25,34 @@ public class OrderServiceImpl implements OrderService {
 	@Autowired
 	private XslTaskMapper xslTaskMapper;
 
+	private static final Logger logger = LoggerFactory.getLogger(OrderServiceImpl.class);
+
 	@Override
 	public void createOrder(CreateOrderReqVo createOrderReqVo) {
+		logger.info("createOrder createOrderReqVo:" + GsonSingle.getGson().toJson(createOrderReqVo));
+
 		XslOrder xslOrder = new XslOrder();
 
 		XslTaskExample xslTaskExample = new XslTaskExample();
 		xslTaskExample.createCriteria().andTaskidEqualTo(createOrderReqVo.getTaskId());
-		List<XslTask> xslTasks = xslTaskMapper.selectByExample(xslTaskExample);
+		try {
+			List<XslTask> xslTasks = xslTaskMapper.selectByExample(xslTaskExample);
 
-		if(!ListUtil.isNotEmpty(xslTasks)){
-			return;
+			if(!ListUtil.isNotEmpty(xslTasks)){
+				return;
+			}
+
+			XslTask xslTask = xslTasks.get(0);
+
+			BeanUtils.copyProperties(xslTask, xslOrder);
+			xslOrder.setOrderid(UUIDUtil.getUUID());
+			xslOrder.setReceiveid(createOrderReqVo.getHunterId());
+			xslOrder.setEnddate(xslTask.getDeadline());
+
+			xslOrderMapper.insertSelective(xslOrder);
+		}catch (Exception e){
+			throw new RuntimeException(e);
 		}
-
-		XslTask xslTask = xslTasks.get(0);
-
-		BeanUtils.copyProperties(xslTask, xslOrder);
-		xslOrder.setOrderid(UUIDUtil.getUUID());
-		xslOrder.setReceiveid(createOrderReqVo.getHunterId());
-		xslOrder.setEnddate(xslTask.getDeadline());
-
-		xslOrderMapper.insertSelective(xslOrder);
 
 	}
 }
